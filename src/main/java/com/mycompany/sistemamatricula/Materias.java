eea727981a4db3546da3f4a254321fb7cae6e1cd
eb031920 <eb775820@gmail.com>
2024-12-04 21:10:53 -0600

    Update Materias.java

--- src/main/java/com/mycompany/sistemamatricula/Materias.java
+++ src/main/java/com/mycompany/sistemamatricula/Materias.java
@@ -8,6 +8,43 @@ package com.mycompany.sistemamatricula;
  *
  * @author UserCP
  */
+
+
+import java.util.ArrayList;
+
 public class Materias {
-    
+    private String nombre;
+    private ArrayList<Profesor> profesores;
+    private ArrayList<String> horarios;
+
+    public Materias(String nombre) {
+        this.nombre = nombre;
+        this.profesores = new ArrayList<>();
+        this.horarios = new ArrayList<>();
+    }
+
+    public void agregarProfesor(Profesor profesor) {
+        profesores.add(profesor);
+    }
+
+    public void agregarHorario(String horario) {
+        horarios.add(horario);
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public ArrayList<Profesor> getProfesores() {
+        return profesores;
+    }
+
+    public ArrayList<String> getHorarios() {
+        return horarios;
+    }
+
+    @Override
+    public String toString() {
+        return nombre;
+    }
 }
