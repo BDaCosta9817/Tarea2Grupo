0245b24f5766219bb40e61fa1185b8138fdc2cda
eb031920 <eb775820@gmail.com>
2024-12-04 21:12:28 -0600

    Update Profesor.java

--- src/main/java/com/mycompany/sistemamatricula/Profesor.java
+++ src/main/java/com/mycompany/sistemamatricula/Profesor.java
@@ -8,6 +8,27 @@ package com.mycompany.sistemamatricula;
  *
  * @author UserCP
  */
+
+
 public class Profesor {
-    
+    private final String nombre;
+    private final String especialidad;
+
+    public Profesor(String nombre, String especialidad) {
+        this.nombre = nombre;
+        this.especialidad = especialidad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getEspecialidad() {
+        return especialidad;
+    }
+
+    @Override
+    public String toString() {
+        return nombre + " - Especialidad: " + especialidad;
+    }
 }
