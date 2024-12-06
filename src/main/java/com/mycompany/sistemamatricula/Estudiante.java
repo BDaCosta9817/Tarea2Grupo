962b9750ab8b3cfb2d8da7f2070eae07e3e351fb
BDaCosta9817 <dacostagomezblanca9998@gmail.com>
2024-12-05 19:58:26 -0600

    Update Estudiante.java

--- src/main/java/com/mycompany/sistemamatricula/Estudiante.java
+++ src/main/java/com/mycompany/sistemamatricula/Estudiante.java
@@ -8,6 +8,39 @@ package com.mycompany.sistemamatricula;
  *
  * @author UserCP
  */
+
+
 public class Estudiante {
-    
+    private final String nombre;
+    private final int edad;
+    private final String genero;
+    private final String carrera;
+
+    public Estudiante(String nombre, int edad, String genero, String carrera) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.genero = genero;
+        this.carrera = carrera;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public String getCarrera() {
+        return carrera;
+    }
+
+    @Override
+    public String toString() {
+        return "Estudiante: " + nombre + ", Edad: " + edad + ", Género: " + genero + ", Carrera: " + carrera;
+    }
 }
