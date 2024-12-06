3a1028308ef1c8a11f290d1ca415bf2585a08851
FerMendoza22 <lfayala@uth.hn>
2024-12-05 23:29:57 -0600

    Update Instituto.java

--- src/main/java/com/mycompany/sistemamatricula/Instituto.java
+++ src/main/java/com/mycompany/sistemamatricula/Instituto.java
@@ -6,8 +6,32 @@ package com.mycompany.sistemamatricula;
 
 /**
  *
- * @author UserCP
+ * @author Luis Fernando Ayala Mendoza
  */
 public class Instituto {
-    
+    // Atributos de la clase Instituto
+    private String nombre;
+    private String direccion;
+
+    // Constructor de la clase Instituto
+    public Instituto(String nombre, String direccion) {
+        this.nombre = nombre;
+        this.direccion = direccion;
+    }
+
+    //Método para obtener el nombre del instituto
+    public String getNombre() {
+        return nombre;
+    }
+
+    // Método para obtener la dirección del instituto
+    public String getDireccion() {
+        return direccion;
+    }
+
+    // Método para obtener una representación en cadena del objeto Instituto
+    @Override
+    public String toString() {
+        return "Instituto: " + nombre + " - Dirección: " + direccion;
+    }
 }
