3e1374be65b157348182c6600eea927d685deff4
BDaCosta9817 <dacostagomezblanca9998@gmail.com>
2024-12-07 21:22:13 -0600

    Update Profesor.java

--- src/main/java/com/mycompany/sistemamatricula/Profesor.java
+++ src/main/java/com/mycompany/sistemamatricula/Profesor.java
@@ -1,13 +1,29 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+//Copyrigth: Ruth Elizabeth Bautista
+// Clase que guarda la información de un profesor: nombre y especialidad.
+// Tiene métodos para obtener esos datos y mostrar la información del profesor.
+
 package com.mycompany.sistemamatricula;
 
-/**
- *
- * @author UserCP
- */
+
 public class Profesor {
-    
+    private final String nombre;
+    private final String especialidad;
+
+    public Profesor(String nombre, String especialidad) {
+        this.nombre = nombre;
+        this.especialidad = especialidad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getEspecialidad() {
+        return especialidad;
+    }
+
+    @Override
+    public String toString() {
+        return nombre + " - Especialidad: " + especialidad;
+    }
 }
