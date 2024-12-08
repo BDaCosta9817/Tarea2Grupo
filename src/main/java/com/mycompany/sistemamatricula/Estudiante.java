5e6dde44beb362ca3b6896cd6bc3bfceaa3993f2
BDaCosta9817 <dacostagomezblanca9998@gmail.com>
2024-12-07 21:13:36 -0600

    Update Estudiante.java

--- src/main/java/com/mycompany/sistemamatricula/Estudiante.java
+++ src/main/java/com/mycompany/sistemamatricula/Estudiante.java
@@ -1,6 +1,6 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ *Fernando Mendoza
+ *Descripcion:esta es nuestra superClase la cual contiene los metodos y atributos para obtener la informacion de nuestro usuario
  */
 package com.mycompany.sistemamatricula;
 
