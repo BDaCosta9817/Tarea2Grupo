7ad95b4f65fbeba76bac08ce5ff5ff5ec5f83f3e
BDaCosta9817 <dacostagomezblanca9998@gmail.com>
2024-12-07 21:17:09 -0600

    Update Inscripcion.java

--- src/main/java/com/mycompany/sistemamatricula/Inscripcion.java
+++ src/main/java/com/mycompany/sistemamatricula/Inscripcion.java
@@ -1,6 +1,6 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ * Blanca Da Costa Gomez Carcamo
+ * Descripcion: la clase inscripcion permite la declaracion y union de cada unos de los apectos que presentaremos en la funcion principal
  */
 package com.mycompany.sistemamatricula;
 
@@ -9,5 +9,24 @@ package com.mycompany.sistemamatricula;
  * @author UserCP
  */
 public class Inscripcion {
-    
+    private Estudiante estudiante;
+    private Materias materia;
+    private Profesor profesor;
+    private String horario;
+
+    public Inscripcion(Estudiante estudiante, Materias materia, Profesor profesor, String horario) {
+        this.estudiante = estudiante;
+        this.materia = materia;
+        this.profesor = profesor;
+        this.horario = horario;
+    }
+
+    @Override
+    public String toString() {
+        return "Inscripción Exitosa!\n" +
+                estudiante + "\n" +
+                "Materia: " + materia.getNombre() + "\n" +
+                "Profesor: " + profesor + "\n" +
+                "Horario: " + horario;
+    }
 }
